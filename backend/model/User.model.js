7c49216a5cef0d21c9562a4125c560a9053cffbd
Sougata Chongder <sougatachongder8@gmail.com>
2025-10-13 13:42:41 +0530

    Update User.model.js

--- backend/model/User.model.js
+++ backend/model/User.model.js
@@ -13,7 +13,8 @@ const userSchema = mongoose.Schema({
     },
     email:{
         type:String,
-        required:true
+        required:true,
+        unique:true
     },
     refreshToken:{
         type:String
